84bea0139433ba4ee4acd7d98e4d447edc6e5932
zhangliang <zhangliang@hoge.cn>
2018-12-20 14:27:17 +0800

    新建dev

---
+++ dev.php
@@ -0,0 +1,38 @@
+<?php 
+
+class Zl
+{
+    
+  public function __construct()
+   {    
+        
+
+   }
+
+ public function is_blind_date()
+    {
+        $member_id = intval($this->user['user_id']);
+        if(!$member_id)
+        {
+            $this->errorOutput(NO_MEMBER_ID);
+        }
+        $sql = "SELECT * FROM ".DB_PREFIX."blind_date WHERE member_id =".$member_id;
+        $ret = $this->db->query_first($sql);
+        if(is_array($ret))
+        {
+            $return = array('message' => 1);
+        }
+        else
+        {
+            $return = array('message' => 0);
+        }
+        $this->addItem($return);
+        $this->output();
+    }
+
+
+
+}
+
+
+?>
